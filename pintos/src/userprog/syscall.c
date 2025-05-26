098eaa10c19d62d2105e8adcbe3c8bdb472498b8
zoklk <bjincheol34@gmail.com>
2025-05-27 02:00:57 +0900

    fix: switch 선언 오류 수정

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -20,21 +20,24 @@ syscall_handler (struct intr_frame *f)
   int syscall_num = *(int*) f->esp;
   switch (syscall_num)
   {
-  case SYS_HALT:
+  case SYS_HALT: {
     /* halt 시스템 콜인 경우 종료 메시지를 출력하지 않고 바로 종료 */
     shutdown_power_off();
     break;
-    
-  case SYS_EXIT:
+    }
+
+  case SYS_EXIT: {
     int status = *((int*)f->esp + 1);
     struct thread *cur = thread_current ();
     cur->exit_status = status;
     thread_exit (); 
     break;
+    }
     
-  default:
+  default: {
     printf ("system call!\n");
     thread_exit ();
     break;
+    }
   }
 }
